7dd2918059c7ceb29fc30791bcd0a29a868ce5e1
Wijnand Korlaar <wijniejs@gmail.com>
2021-01-17 22:44:06 +0100

    w

--- src/components/Acccordion/Accordion.js
+++ src/components/Acccordion/Accordion.js
@@ -5,7 +5,7 @@ import React from 'react';
 const paragraph1 = `2 years of bachelor marketing,  3 years as sales leader in a franchise, responsible for controlling staff and appearence /  of articles. Since 2018 process operater and part time student.`
 const paragraph2 = 'Altough I chose marketing as study direction I did always have a passion for computers and technology. I am very competetive , and with an analytical mindset I find the best parts of myself in software development  '
 const paragraph3 = 'Novice webdev with Javascript. Has knowledge of Nextjs , Gatsby , Webpack, Nodejs with express, Oauth, Redux, GraphQl, Firebase, and more.. '
-const paragraph4 = "korlaarwijnand@hotmail.com  Phone: +31651399175"
+const paragraph4 = "korlaarwijnand@hotmail.com,  GIT: https://github.com/WijnandK , Phone: +31651399175"
 
 const data = [
   {
